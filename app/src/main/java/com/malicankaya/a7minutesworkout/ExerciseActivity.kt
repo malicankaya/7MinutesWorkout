71e7badda3319ce864f808df991ec80e809d8aad
malicankaya <malicankaya97@gmail.com>
2022-09-23 01:51:20 +0300

    adding exercisetimer

--- app/src/main/java/com/malicankaya/a7minutesworkout/ExerciseActivity.kt
+++ app/src/main/java/com/malicankaya/a7minutesworkout/ExerciseActivity.kt
@@ -3,14 +3,24 @@ package com.malicankaya.a7minutesworkout
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.view.View
 import android.widget.Toast
 import com.malicankaya.a7minutesworkout.databinding.ActivityExerciseBinding
+import androidx.constraintlayout.widget.ConstraintSet
+
+
+
 
 class ExerciseActivity : AppCompatActivity() {
     var binding: ActivityExerciseBinding? = null
-    private var countDownTimer: CountDownTimer? = null
-    private var timerProgressSec = 0
-    private var timerSec: Long = 10
+    private var readyCountDownTimer: CountDownTimer? = null
+    private var readyTimerProgressSec = 0
+    private var readyTimerSec: Long = 10
+
+    private var exerciseCountDownTimer: CountDownTimer? = null
+    private var exerciseTimerProgressSec = 0
+    private var exerciseTimerSec: Long = 30
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityExerciseBinding.inflate(layoutInflater)
@@ -21,7 +31,7 @@ class ExerciseActivity : AppCompatActivity() {
             supportActionBar?.setDisplayHomeAsUpEnabled(true)
         }
         binding?.toolbarExercise?.setNavigationOnClickListener {
-            countDownTimer?.cancel()
+            readyCountDownTimer?.cancel()
             onBackPressed()
         }
 
@@ -30,27 +40,67 @@ class ExerciseActivity : AppCompatActivity() {
     }
 
     private fun setTimer() {
-        if (countDownTimer != null){
-            countDownTimer?.cancel()
-            timerProgressSec = 0
+        if (readyCountDownTimer != null){
+            readyCountDownTimer?.cancel()
+            readyTimerProgressSec = 0
         }
         setTimerProgressBar()
     }
 
     private fun setTimerProgressBar() {
-        binding?.progressBar?.progress = timerSec.toInt()
-        binding?.tvCount?.text = timerSec.toString()
+        binding?.progressBarReady?.progress = readyTimerSec.toInt()
+        binding?.tvReadyCount?.text = readyTimerSec.toString()
 
-        countDownTimer = object : CountDownTimer(timerSec * 1000, 1000) {
+        readyCountDownTimer = object : CountDownTimer(readyTimerSec * 1000, 1000) {
             override fun onTick(millisUntilFinished: Long) {
-                timerProgressSec++
-                binding?.progressBar?.progress = timerSec.toInt() - timerProgressSec
-                binding?.tvCount?.text = (timerSec.toInt() - timerProgressSec).toString()
+                readyTimerProgressSec++
+                binding?.progressBarReady?.progress = readyTimerSec.toInt() - readyTimerProgressSec
+                binding?.tvReadyCount?.text = (readyTimerSec.toInt() - readyTimerProgressSec).toString()
             }
             override fun onFinish() {
-                Toast.makeText(this@ExerciseActivity,"Now, we will start the exercise.",
-                Toast.LENGTH_SHORT).show()
+                exerciseSetTimer()
             }
         }.start()
     }
+
+    private fun exerciseSetTimer() {
+        binding?.flReady?.visibility = View.GONE
+        val constraintSet = ConstraintSet()
+        constraintSet.clone(binding?.root)
+        constraintSet.connect(binding?.tvExercise!!.id,ConstraintSet.BOTTOM,binding?.flExercise!!.id,ConstraintSet.TOP,0);
+        constraintSet.applyTo(binding?.root)
+        binding?.tvExercise?.text = "Exercise name"
+        binding?.flExercise?.visibility = View.VISIBLE
+
+        if (exerciseCountDownTimer != null){
+            exerciseCountDownTimer?.cancel()
+            exerciseTimerProgressSec = 0
+        }
+
+        exerciseSetTimerProgressBar()
+    }
+
+    private fun exerciseSetTimerProgressBar() {
+        binding?.exerciseProgressBar?.progress = exerciseTimerSec.toInt()
+        binding?.tvExerciseCount?.text = exerciseTimerSec.toString()
+
+        readyCountDownTimer = object : CountDownTimer(exerciseTimerSec * 1000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                exerciseTimerProgressSec++
+                binding?.exerciseProgressBar?.progress = exerciseTimerSec.toInt() - exerciseTimerProgressSec
+                binding?.tvExerciseCount?.text = (exerciseTimerSec.toInt() - exerciseTimerProgressSec).toString()
+            }
+            override fun onFinish() {
+                Toast.makeText(this@ExerciseActivity,"Now, we will start the exercise.",
+                    Toast.LENGTH_SHORT).show()
+            }
+        }.start()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        binding = null
+    }
+
 }
